afcbf6f60599552dba3a5fce4604fab35a5dfb51
SigmaZardan <bibekbhujel077@gmail.com>
2024-11-09 16:39:59 +0545

    Initial Commit

---
+++ Swift14Basics/Swift14BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift14BasicsApp.swift
+//  Swift14Basics
+//
+//  Created by Bibek Bhujel on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift14BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift14BasicsTests/Swift14BasicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift14BasicsTests.swift
+//  Swift14BasicsTests
+//
+//  Created by Bibek Bhujel on 09/11/2024.
+//
+
+import Testing
+@testable import Swift14Basics
+
+struct Swift14BasicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
